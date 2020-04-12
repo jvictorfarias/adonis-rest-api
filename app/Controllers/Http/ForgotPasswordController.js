ec90778f364b33cb33b9b063272e6e5a97e93fe1
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2020-04-12 19:55:57 -0300

    controller: Forgot Password Controller

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,7 +1,26 @@
 'use strict'
 
+const crypto = require('crypto')
+const User = use('App/Models/User')
+
 class ForgotPasswordController {
-  async store() {}
+  async store({ request, response }) {
+    const email = request.input('email')
+
+    try {
+      // OrFail retorna mensagem de erro
+      const user = await User.findByOrFail('email', email)
+
+      user.token = crypto.randomBytes(10).toString('hex')
+      user.token_created_at = new Date()
+
+      await user.save()
+    } catch (error) {
+      return response
+        .status(error.status)
+        .send({ error: { message: 'Email not found' } })
+    }
+  }
 }
 
 module.exports = ForgotPasswordController
--- start/routes.js
+++ start/routes.js
@@ -19,3 +19,5 @@ const Route = use('Route')
 Route.post('users', 'UserController.store')
 
 Route.post('sessions', 'SessionController.store')
+
+Route.post('passwords', 'ForgotPasswordController.store')
